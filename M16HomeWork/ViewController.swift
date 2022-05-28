af0abf587a3f4bd6ae6d5828a2cd02c3abf6a5c6
Chamov Pavel <chamov10crew@gmail.com>
2022-05-28 20:11:30 +0300

    main

---
+++ M16HomeWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  M16HomeWork
+//
+//  Created by Pavel on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    func printing() {
+        print("hello Skillbox")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        printing()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
